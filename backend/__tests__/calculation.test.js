705981a7de68ebbbaa8931fcbcac8a1c5be7dce2
Julian Grey <julianrgrey@gmail.com>
2025-10-29 13:16:40 +0000

    Change timePerLap to accept minutes and seconds

--- backend/__tests__/calculation.test.js
+++ backend/__tests__/calculation.test.js
@@ -2,13 +2,13 @@ const { calculateFuelForRace } = require('../calculation.cjs')
 
 describe('Calculating the fuel for race', () => {
   it('should give the amount of fuel required for a race, and the number of laps to complete a 15 minute race', () => {
-    const raceStats = calculateFuelForRace(2, { minutes: 15 }, 90)
+    const raceStats = calculateFuelForRace(2, { raceMinutes: 15 }, { lapMinutes: 1, lapSeconds: 30 })
 
     expect(raceStats).toEqual({numLaps: 10, raceFuel: 20})
   })
 
   it('should give the amount of fuel required for a race, and the number of laps to complete a 1 hour race', () => {
-    const raceStats = calculateFuelForRace(2, { hours: 1 }, 90)
+    const raceStats = calculateFuelForRace(2, { raceHours: 1 }, { lapMinutes: 1, lapSeconds: 30 })
 
     expect(raceStats).toEqual({numLaps: 40, raceFuel: 80})
   })
--- backend/calculation.cjs
+++ backend/calculation.cjs
@@ -1,15 +1,21 @@
 /**
  * @typedef {Object} RaceDuration
- * @property {number} [hours=0]
- * @property {number} [minutes=0]
- * @property {number} [seconds=0]
+ * @property {number} [raceHours=0]
+ * @property {number} [raceMinutes=0]
+ * @property {number} [raceSeconds=0]
+ */
+
+/**
+ * @typedef {Object} LapTime
+ * @property {number} [lapMinutes=0]
+ * @property {number} [lapSeconds=0]
  */
 
 /**
  * Determine how much fuel is required for a given race.
  * @param {number} fuelPerLap How much fuel is required to complete one lap
  * @param {RaceDuration} raceDuration How long the race will run
- * @param {number} timePerLap How long it takes to complete a lap
+ * @param {LapTime} timePerLap How long it takes to complete a lap
  * @returns {{numLaps: number, raceFuel: number}}
  */
 function calculateFuelForRace(
@@ -17,9 +23,11 @@ function calculateFuelForRace(
   raceDuration,
   timePerLap,
 ) {
-  const { hours = 0, minutes = 0, seconds = 0 } = raceDuration
-  const duration = (hours * 3600) + (minutes * 60) + seconds
-  const numLaps = (duration / timePerLap)
+  const { raceHours = 0, raceMinutes = 0, raceSeconds = 0 } = raceDuration
+  const { lapMinutes = 0, lapSeconds = 0 } = timePerLap
+  const duration = (raceHours * 3600) + (raceMinutes * 60) + raceSeconds
+  const lapTime = (lapMinutes * 60) + lapSeconds
+  const numLaps = (duration / lapTime)
   const raceFuel = (numLaps * fuelPerLap)
 
   return ({
