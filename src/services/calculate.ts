6d76cb5303e542a8649137706055c2a4ecab102c
Julian Grey <julianrgrey@gmail.com>
2025-11-05 23:51:45 +0000

    Create interfaces for calculate service

--- src/services/calculate.ts
+++ src/services/calculate.ts
@@ -1,8 +1,25 @@
 import axios from 'axios'
 
+interface LapDuration {
+  lapTimeMinutes: number;
+  lapTimeSeconds: number;
+}
+
+interface RaceDuration {
+  raceTimeHours: number;
+  raceTimeMinutes: number;
+}
+
+interface RaceInfo {
+  lapEnergy?: number;
+  lapFuel: number;
+  lapDuration: LapDuration;
+  raceDuration: RaceDuration
+}
+
 const apiUrl = 'http://localhost:3000/api'
 
-export const calculate = async (raceInfo) => {
+export const calculate = async (raceInfo: RaceInfo) => {
   const { lapDuration, lapFuel, raceDuration, lapEnergy } = raceInfo
 
   const {data} = await axios.post(`${apiUrl}/calculate-race-fuel`, {
