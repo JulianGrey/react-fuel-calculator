e2a8d831311e9cfcd86f50fca17400c17062be0a
Julian Grey <julianrgrey@gmail.com>
2025-11-10 10:06:04 +0000

    Fix spacing

--- src/services/calculate.ts
+++ src/services/calculate.ts
@@ -22,7 +22,7 @@ const apiUrl = 'http://localhost:3000/api'
 export const calculate = async (raceInfo: RaceInfo) => {
   const { lapDuration, lapFuel, raceDuration, lapEnergy } = raceInfo
 
-  const {data} = await axios.post(`${apiUrl}/calculate-race-fuel`, {
+  const { data } = await axios.post(`${apiUrl}/calculate-race-fuel`, {
     lapDuration,
     lapFuel,
     raceDuration,
